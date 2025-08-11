fea2e2a084887188fbf9d7a066d6db61655720c1
chinimuruhi <31010801+chinimuruhi@users.noreply.github.com>
2025-08-11 22:57:16 +0900

    [Add] Github workflowの追加

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,25 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000
+export default defineConfig(({ mode }) => {
+  // baseプロパティに設定する値
+  let base = "/"
+
+  // 本番時に適用させるbaseの値
+  if (mode === "production") {
+    base = "/INFINITAS-ScoreViewer/"
   }
-});
+
+  return {
+    plugins: [react()],
+    // アセットなどのパスを変換するベースとなるパス
+    base: base,
+    // ビルドの出力先
+    build: {
+      outDir: 'dict'
+    },
+    server: {
+      port: 3000
+    }
+  };
+})
